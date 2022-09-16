495be466c30bafccd8dfa3f95cbe77bbbe853f5a
YourZombieMop <68197922+YourZombieMop@users.noreply.github.com>
2022-09-16 16:37:25 -0500

    Assignment 1

---
+++ main.js
@@ -0,0 +1,19 @@
+// Q1 Q2
+let myNumber = 5;
+// Q3
+let myOtherNumber = 5;
+// Q4
+let myTotal = myNumber * myOtherNumber;
+// Q5
+let myName = "";
+let myCombo = String(myNumber + myOtherNumber);
+// Q6
+let myOtherCombo = myNumber + myOtherNumber;
+// Q7
+let myRemainder = myNumber % 5;
+// Q8
+let i = 2;
+// Q9
+let int = 4;
+// Q10
+let myBoolean = false;
